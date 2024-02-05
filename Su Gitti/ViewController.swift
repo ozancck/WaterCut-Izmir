6bb5745effc2fccbd5b6010e020bae43eb36c846
Ozan Çiçek <ozan.cicek@isteportal.com.tr>
2024-02-05 18:05:57 +0300

    Initial Commit

---
+++ Su Gitti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Su Gitti
+//
+//  Created by Ozan Çiçek on 5.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
